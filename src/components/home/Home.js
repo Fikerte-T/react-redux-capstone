e47f60226f23439645f22346dfcc37af04099777
fikerte <tofikertesfaye@gmail.com>
2022-02-11 21:19:01 +0300

    Update folder structure

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { getDataFromApi } from '../redux/home/home';
+import { getDataFromApi } from '../../redux/home/home';
 import rightArrow from '../images/arrow_circle_right_white_24dp.svg';
 
 // const selectedData = (state) => state.homeReducer;
